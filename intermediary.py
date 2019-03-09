9bcf17b2720bb3944227fb272f9f6e56b0428793
Kenton Lam <kenton_lam@outlook.com>
2019-03-09 14:08:00 +1000

    Working..

--- intermediary.py
+++ intermediary.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import json
 import enum
+import csv
 
 routes_to_strip = {} 
 
@@ -24,7 +25,7 @@ destinations = {
     'Cleveland': None
 }
 
-class RouteStrip(enum):
+class RouteStrip(enum.Enum):
     Red = enum.auto() 
     DarkBlue = enum.auto() 
     Yellow = enum.auto()
@@ -62,15 +63,34 @@ canonical_trips = {
 }
 
 with open('SEQ_GTFS/trips.txt') as f:
-    trips = json.load(f)
+    trips = list(csv.DictReader(f))
 
 with open('SEQ_GTFS/stop_times.txt') as f:
-    stop_times = json.load(f)
+    stop_times = f.read()
 
 def find_trip_on_route(route):
-    pass
+    for t in trips:
+        if t['route_id'] == route:
+            return t['trip_id']
+
+def find_stops_on_trip(trip):
+    stops = set()
+    for l in stop_times:
+        l = l.split(',')
+        if l[0] == trip:
+            stops.add(l[3])
+        elif stops:
+            return stops
+    return stops
+
 
 for strip, routes in canonical_trips.items():
+    stops = set()
+    for r in routes:
+        print(strip, r)
+        t = find_trip_on_route(r)
+        print(t)
+        print(find_stops_on_trip(t))
 
 
 
