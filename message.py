911d0252898dfcc23ad8cf54ab1826f45ad310fd
Kenton Lam <kenton_lam@outlook.com>
2019-03-10 11:28:19 +1000

    Updated message.py to accept int and added clear.

--- message.py
+++ message.py
@@ -12,15 +12,19 @@ class MessageBuilder:
         """Formats the message. Used before sending a message to the client"""
         return b'!%b$' % self._message
 
+    def clear_message(self) -> None:
+        """Clears the current message."""
+        self._message = bytes()
+
 
 def build_off_command(id: int) -> bytes:
     return b'0,%d;' % id
 
-def build_on_command(id: int, color: str) -> bytes:
-    return b'1,%d,%x;' % (id, int(color, 16))
+def build_on_command(id: int, color: int) -> bytes:
+    return b'1,%d,%x;' % (id, color)
 
-def build_flash_command(id: int, color: str) -> bytes:
-    return b'2,%d,%x,255;' % (id, int(color, 16))
+def build_flash_command(id: int, color: int) -> bytes:
+    return b'2,%d,%x,255;' % (id, color)
 
-def build_fade_command(id: int, color: str, percentage: int) -> bytes:
-    return b'3,%d,%x,%d;' % (id, int(color, 16), percentage)
+def build_fade_command(id: int, color: int, percentage: int) -> bytes:
+    return b'3,%d,%x,%d;' % (id, color, percentage)
