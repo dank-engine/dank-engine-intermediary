4f3c00172954e6713f26dac8c575dfd495837018
Kenton Lam <kenton_lam@outlook.com>
2019-03-16 17:44:15 +1000

    On command now accepts percent.

--- message.py
+++ message.py
@@ -20,8 +20,8 @@ class MessageBuilder:
 def build_off_command(id: int) -> bytes:
     return b'0,%d,0,0;' % id
 
-def build_on_command(id: int, color: int) -> bytes:
-    return b'1,%d,%d,0;' % (id, color)
+def build_on_command(id: int, color: int, percent: int=255) -> bytes:
+    return b'1,%d,%d,%d;' % (id, color, percent)
 
 def build_flash_command(id: int, color: int) -> bytes:
     return b'2,%d,%d,0;' % (id, color)
